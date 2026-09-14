dd5f739aaa6993c95b76a31ab75b86b857af956d
buitanloc-2310 <buitanloccon@gmail.com>
2026-09-14 11:04:24 +0700

    Delete SFN-SLC-VIPPRO directory

--- SFN-SLC-VIPPRO/functions/api/[[path]].js
+++
@@ -1,8 +0,0 @@
-import { handleApiRequest } from '../../src/index.js';
-
-export async function onRequest(context) {
-  return handleApiRequest(context.request, context.env, {
-    waitUntil: (promise) => context.waitUntil(promise),
-    passThroughOnException: () => context.passThroughOnException?.()
-  });
-}
